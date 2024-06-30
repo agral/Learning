2bf0972e55e40e3260f6f9b32885624edb1395de
agral <adam@gralin.ski>
2024-06-30 20:42:24 +0200

    [Golang] Pomodoro: implement rudimentary timing

--- Programming/Golang/PomodoroTimer/main.go
+++ Programming/Golang/PomodoroTimer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 const help = `pomodoro - run an interactive pomodoro session in a terminal
@@ -20,10 +21,31 @@ const help = `pomodoro - run an interactive pomodoro session in a terminal
     author: (c) 2024 https://gralin.ski
     license: MIT`
 
+// const workPeriodSeconds = 25 * 60
+// const restPeriodSeconds = 5 * 60
+const workPeriodSeconds = 25
+const restPeriodSeconds = 5
+const totalPeriodSeconds = workPeriodSeconds + restPeriodSeconds
+
+func print_timer(timer int) {
+	if timer < workPeriodSeconds {
+		fmt.Println("Work")
+		fmt.Println(fmt.Sprintf("[%d/%d]", timer, workPeriodSeconds))
+	} else {
+		fmt.Println("Rest")
+		fmt.Println(fmt.Sprintf("[%d/%d]", timer-workPeriodSeconds, restPeriodSeconds))
+	}
+	fmt.Println(timer)
+}
+
 func print_help() {
 	fmt.Println(help)
 }
 
 func main() {
-	print_help()
+	for timer := 0; timer < totalPeriodSeconds; timer++ {
+		print_timer(timer)
+		time.Sleep(time.Second)
+	}
+	fmt.Println("Done!")
 }
