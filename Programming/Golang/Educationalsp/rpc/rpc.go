0891c08c7c9c837a9e4ffffc5fff092ae4d44087
agral <adam@gralin.ski>
2024-07-01 21:53:43 +0200

    Implement EncodeMessage properly
    Tests pass.

--- Programming/Golang/Educationalsp/rpc/rpc.go
+++ Programming/Golang/Educationalsp/rpc/rpc.go
@@ -1,5 +1,14 @@
 package rpc
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 func EncodeMessage(msg any) string {
-	return ""
+	content, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
 }
