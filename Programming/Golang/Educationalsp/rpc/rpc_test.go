b3f9740d64559963689f0bc410f05351e5cbc28e
agral <adam@gralin.ski>
2024-07-02 19:03:17 +0200

    Add failing test for DecodeMessage

--- Programming/Golang/Educationalsp/rpc/rpc_test.go
+++ Programming/Golang/Educationalsp/rpc/rpc_test.go
@@ -16,3 +16,17 @@ func TestEncodeMessage(t *testing.T) {
 		t.Fatalf("Expected: %s, actual: %s", expected, actual)
 	}
 }
+
+func TestDecodeMessage(t *testing.T) {
+	incomingMessage := "Content-Length: 15\r\n\r\n{\"Method\":\"hi\"}"
+	method, contentLength, err := rpc.DecodeMessage([]byte(incomingMessage))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if contentLength != 15 {
+		t.Fatalf("Wrong Content-Length: expected 15, got: %d", contentLength)
+	}
+	if method != "hi" {
+		t.Fatalf("Wrong method: expected \"hi\", got \"%s\"", method)
+	}
+}
