de80783417bb53d42879dc340498e5033132753e
agral <adam@gralin.ski>
2024-07-03 09:33:50 +0200

    Add logger to main.go

--- Programming/Golang/Educationalsp/main.go
+++ Programming/Golang/Educationalsp/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bufio"
 	"educationalsp/rpc"
+	"log"
 	"os"
 )
 
 func main() {
+	logger := getLogger("/tmp/educationalsp.txt")
+	logger.Println("Educationalsp is starting")
 	scanner := bufio.NewScanner(os.Stdin)
-
 	scanner.Split(rpc.Split)
 
 	for scanner.Scan() {
@@ -18,4 +20,12 @@ func main() {
 }
 
 func handleMessage(_ any) {
+}
+
+func getLogger(filename string) *log.Logger {
+	logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("Bad log file given")
+	}
+	return log.New(logfile, "[educationalsp]", log.Ldate|log.Ltime|log.Lshortfile)
 }
