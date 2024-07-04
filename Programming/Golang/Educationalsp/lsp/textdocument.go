93076a7722605165bc33c79e098c9dfe9e210879
agral <adam@gralin.ski>
2024-07-04 11:51:52 +0200

    Synchronize text state
    Makes educationalsp aware of didChange messages from the editor.

---
+++ Programming/Golang/Educationalsp/lsp/textdocument.go
@@ -0,0 +1,11 @@
+package lsp
+
+type TextDocumentItem struct {
+	Uri        string `json:"uri"`
+	LanguageId string `json:"languageId"`
+
+	// The version number of this TextDocumentItem.
+	// It will be increased after every change, including undo&redo.
+	Version int    `json:"version"`
+	Text    string `json:"text"`
+}
