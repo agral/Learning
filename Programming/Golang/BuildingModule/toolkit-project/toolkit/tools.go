a9c546708efdb6eeb2faa9608b6df4e06466e3e6
agral <adam@gralin.ski>
2024-07-08 19:07:42 +0200

    Provide function handling upload of files

--- Programming/Golang/BuildingModule/toolkit-project/toolkit/tools.go
+++ Programming/Golang/BuildingModule/toolkit-project/toolkit/tools.go
@@ -1,10 +1,21 @@
 package toolkit
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
 
 type Tools struct {
+	MaxFileSize      int64
+	AllowedFileTypes []string
 }
 
 func (t *Tools) RandomString(length int) string {
@@ -16,4 +27,96 @@ func (t *Tools) RandomString(length int) string {
 	}
 
 	return string(s)
+}
+
+// Stores information about an uploaded file.
+type UploadedFile struct {
+	NewFileName      string
+	OriginalFileName string
+	FileSize         int64
+}
+
+func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
+	renameFile := true
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	var uploadedFiles []*UploadedFile
+	if t.MaxFileSize == 0 {
+		t.MaxFileSize = 1024 * 1024 * 1024 // 1 GiB
+	}
+	err := r.ParseMultipartForm(t.MaxFileSize)
+	if err != nil {
+		return nil, errors.New("The file is too big")
+	}
+
+	for _, fHeaders := range r.MultipartForm.File {
+		for _, header := range fHeaders {
+			uploadedFiles, err = func(uploadedFiles []*UploadedFile) ([]*UploadedFile, error) {
+				var uploadedFile UploadedFile
+				infile, err := header.Open()
+				if err != nil {
+					return nil, err
+				}
+				defer infile.Close()
+
+				buff := make([]byte, 512)
+				if _, err := infile.Read(buff); err != nil {
+					return nil, err
+				}
+
+				isAllowed := false
+				fileType := http.DetectContentType(buff)
+
+				if len(t.AllowedFileTypes) > 0 {
+					for _, x := range t.AllowedFileTypes {
+						if strings.EqualFold(fileType, x) {
+							isAllowed = true
+						}
+					}
+				} else {
+					// When allowlist is empty, all file types are considered allowed.
+					isAllowed = true
+				}
+
+				if !isAllowed {
+					return nil, errors.New("The uploaded file type is not permitted")
+				}
+
+				_, err = infile.Seek(0, 0)
+				if err != nil {
+					return nil, err
+				}
+
+				if renameFile {
+					uploadedFile.NewFileName = fmt.Sprintf("%s%s", t.RandomString(25),
+						filepath.Ext(header.Filename))
+				} else {
+					uploadedFile.NewFileName = header.Filename
+				}
+
+				var outfile *os.File
+				defer outfile.Close()
+				if outfile, err = os.Create(filepath.Join(uploadDir, uploadedFile.NewFileName)); err != nil {
+					return nil, err
+				} else {
+					fileSize, err := io.Copy(outfile, infile)
+					if err != nil {
+						return nil, err
+					}
+					uploadedFile.FileSize = fileSize
+				}
+
+				uploadedFiles = append(uploadedFiles, &uploadedFile)
+				return uploadedFiles, nil
+			}(uploadedFiles)
+
+			if err != nil {
+				return uploadedFiles, err
+			}
+		}
+	}
+
+	return uploadedFiles, nil
 }
