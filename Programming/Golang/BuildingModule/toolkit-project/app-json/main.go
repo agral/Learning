4552ac45302ddbe8a4133144a2fe94738a9c09eb
agral <adam@gralin.ski>
2024-07-12 07:02:46 +0200

    Prepare web app, handle JSONs via POST method

---
+++ Programming/Golang/BuildingModule/toolkit-project/app-json/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"toolkit"
+)
+
+type RequestPayload struct {
+	Action  string `json:"action"`
+	Message string `json:"message"`
+}
+
+type ResponsePayload struct {
+	Message    string `json:"message"`
+	StatusCode int    `json:"status_code,omitempty"`
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/receive-post", receivePost)
+	mux.HandleFunc("/remote-service", remoteService)
+
+	log.Println("Starting the server...")
+
+	err := http.ListenAndServe(":8081", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func receivePost(w http.ResponseWriter, r *http.Request) {
+	var requestPayload RequestPayload
+	var t toolkit.Tools
+	err := t.ReadJson(w, r, &requestPayload)
+	if err != nil {
+		t.ErrorJson(w, err)
+		return
+	}
+
+	responsePayload := ResponsePayload{
+		Message: "The handler works correctly",
+	}
+	err = t.WriteJson(w, http.StatusAccepted, responsePayload)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func remoteService(w http.ResponseWriter, r *http.Request) {
+}
