1cf07b44daa9fba53564d79162e08fae44a1dee4
agral <adam@gralin.ski>
2024-07-14 20:21:50 +0200

    Add goroutines with channel
    Improves the endpoint by adding new functionality:
    - notifying all clients
    - listening for broadcasts
    
    Adds a dedicated channel and a simple go routine
    that handles the broadcasting.

--- Programming/Golang/Websockets/Chat/cmd/web/main.go
+++ Programming/Golang/Websockets/Chat/cmd/web/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"gochat/internal/handlers"
 	"log"
 	"net/http"
 )
 
 func main() {
 	mux := routes()
+
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
 	const port = ":8080"
 	log.Printf("Starting the server on port %s\n", port)
 	_ = http.ListenAndServe(port, mux)
