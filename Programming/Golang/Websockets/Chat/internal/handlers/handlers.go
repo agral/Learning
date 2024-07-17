793c60226d56335264c71d1e69ce38b6d5ff2831
agral <adam@gralin.ski>
2024-07-17 22:22:40 +0200

    Implement handling of chat messages

--- Programming/Golang/Websockets/Chat/internal/handlers/handlers.go
+++ Programming/Golang/Websockets/Chat/internal/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"sort"
@@ -107,6 +108,10 @@ func ListenToWsChannel() {
 			delete(clients, e.Conn)
 			users := getUserList()
 			response.ConnectedUsers = users
+			broadcastToAll(response)
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</strong>: %s", e.Username, e.Message)
 			broadcastToAll(response)
 		}
 	}
