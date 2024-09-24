8b41bb04072d319ef10c7702d39afeeaa7cd7b8f
agral <adam@gralin.ski>
2024-09-24 15:23:28 +0200

    Start p5.js project - dragon curve fractal

---
+++ Programming/Javascript/p5js/dragon_curve/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(800, 600);
+}
+
+function draw() {
+    //let a = p5.Vector(100, 100);
+    //let b = p5.Vector(300, 300);
+    background(220);
+    point(240, 320);
+}
