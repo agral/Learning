786ac6cc95e579b81d9ffcf4323fc0bd8b39796b
agral <adam@gralin.ski>
2024-12-30 22:50:06 +0100

    LearningGo: add sidenote about randomized order exceptions

--- Programming/Golang/BodnerLearningGo/ch4/map_iteration_order_varies.go
+++ Programming/Golang/BodnerLearningGo/ch4/map_iteration_order_varies.go
@@ -13,6 +13,7 @@ func main() {
 	// when iterating over map's key-value pairs with for-range,
 	// the iteration order varies - keys are randomly accessed.
 	// So we're likely to see differently ordered outputs in each loop:
+	fmt.Println("Expecting randomized output there:")
 	for i := 0; i < 3; i++ {
 		fmt.Printf("Loop #%d\n", i)
 		for k, v := range m {
@@ -20,4 +21,11 @@ func main() {
 		}
 	}
 	// (this is a security feature, against Hash DoS and other issues)
+
+	// But to make debugging simpler, the formatting functions always output
+	// maps with their keys in ascending sorting order when printed as a whole.
+	fmt.Println("Expecting fixed output, sorted in ascending keys order there:")
+	for i := 0; i < 3; i++ {
+		fmt.Println(m)
+	}
 }
