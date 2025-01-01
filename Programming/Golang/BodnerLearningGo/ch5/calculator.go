da46e570bc4c4a5d25f4e17edd97dae031ef83cc
agral <adam@gralin.ski>
2025-01-01 19:37:56 +0100

    LearningGo: modify calculator to error on div-by-zero

--- Programming/Golang/BodnerLearningGo/ch5/calculator.go
+++ Programming/Golang/BodnerLearningGo/ch5/calculator.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 )
 
-func add(lhs int, rhs int) int { return lhs + rhs }
-func sub(lhs int, rhs int) int { return lhs - rhs }
-func mul(lhs int, rhs int) int { return lhs * rhs }
-func div(lhs int, rhs int) int { return lhs / rhs }
+func add(lhs int, rhs int) (int, error) { return lhs + rhs, nil }
+func sub(lhs int, rhs int) (int, error) { return lhs - rhs, nil }
+func mul(lhs int, rhs int) (int, error) { return lhs * rhs, nil }
+func div(lhs int, rhs int) (int, error) {
+	if rhs == 0 {
+		return 0, errors.New("Division by zero")
+	}
+	return lhs / rhs, nil
+}
 
-var opMap = map[string]func(int, int) int{
+var opMap = map[string]func(int, int) (int, error){
 	"+": add,
 	"-": sub,
 	"*": mul,
@@ -26,6 +32,8 @@ func tryCalc() {
 		{"2", "%", "3"},
 		{"two", "+", "three"},
 		{"5"},
+		{"4", "/", "0"},
+		{"0", "/", "0"},
 	}
 
 	for _, expression := range expressions {
@@ -49,7 +57,11 @@ func tryCalc() {
 			fmt.Println(err)
 			continue
 		}
-		result := opFunc(lhs, rhs)
+		result, err := opFunc(lhs, rhs)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 		fmt.Println(result)
 	}
 }
