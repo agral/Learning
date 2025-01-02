8adb507fe265f95eceab8ff601a4954182937522
agral <adam@gralin.ski>
2025-01-02 17:43:30 +0100

    LearningGo: Remove excessive code

--- Programming/Golang/BodnerLearningGo/ch6/exercises.go
+++ Programming/Golang/BodnerLearningGo/ch6/exercises.go
@@ -80,18 +80,7 @@ func ch6_ex2() {
 	fmt.Printf("After calling GrowSlice(), `sl2`=%v\n", sl2)
 }
 
-func ch6_ex3() {
-	// Write a program that builds a []Person with 10,000,000 entries (they could all
-	// be the same names and ages). See how long it takes to run. Change the value
-	// of GOGC and see how that affects the time it takes for the program to complete.
-	// Set the environment variable GODEBUG=gctrace=1 to see when garbage collections
-	// happen and see how changing GOGC changes the number of garbage collections.
-	// What happens if you create the slice with a capacity of 10,000,000?
-	db := make([]Person
-}
-
 func main() {
 	ch6_ex1()
 	ch6_ex2()
-	ch6_ex3()
 }
