2b3424e77ffe91b6125b1c012baff1698d1da183
agral <adam@gralin.ski>
2025-01-19 21:23:35 +0100

    Plan out the producer-consumer code

---
+++ Programming/Golang/ConcurrencyGo/024_producer_consumer/main.go
@@ -0,0 +1,17 @@
+package main
+
+const NumberOfPizzas = 10
+
+func main() {
+	// seed the pseudorandom number generator
+
+	// print out a message
+
+	// create a producer
+
+	// run the producer in the background (as its own goroutine)
+
+	// create and run a consumer
+
+	// print out the ending message
+}
