ab724276f00b7f3ebfeb23ff188e97f262c208c0
agral <adam@gralin.ski>
2025-01-19 21:27:58 +0100

    Add necessary types and vars to producer-consumer

--- Programming/Golang/ConcurrencyGo/024_producer_consumer/main.go
+++ Programming/Golang/ConcurrencyGo/024_producer_consumer/main.go
@@ -2,6 +2,19 @@ package main
 
 const NumberOfPizzas = 10
 
+var totalPizzasMade, totalPizzasFailed, total int
+
+type PizzaOrder struct {
+	pizzaNumber int
+	message     string
+	isSuccess   bool
+}
+
+type Producer struct {
+	data chan PizzaOrder
+	quit chan chan error // this channel holds channel of errors
+}
+
 func main() {
 	// seed the pseudorandom number generator
 
