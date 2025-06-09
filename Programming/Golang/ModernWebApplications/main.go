8f8d05e0174b7844887a98cbd185a3c08c8ae295
agral <adam@gralin.ski>
2025-06-09 12:33:12 +0200

    Add another route; the about page

--- Programming/Golang/ModernWebApplications/main.go
+++ Programming/Golang/ModernWebApplications/main.go
@@ -5,9 +5,16 @@ import (
 	"net/http"
 )
 
+func Home(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "Hello, World!")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "This is the about page.")
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintf(w, "Hello, World!")
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 	http.ListenAndServe(":31337", nil)
 }
