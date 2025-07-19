ef608c84d79895a333038fabd87caf0e9f43e1d3
agral <adam@gralin.ski>
2025-07-19 11:12:02 +0200

    Add a crude templating system

--- Programming/Golang/ModernWebApplications/main.go
+++ Programming/Golang/ModernWebApplications/main.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const PORT_NUMBER = ":31337"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "Hello, World!")
+	renderTemplate(w, "home.page.tmpl")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "This is the about page.")
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return
+	}
 }
 
 func main() {
