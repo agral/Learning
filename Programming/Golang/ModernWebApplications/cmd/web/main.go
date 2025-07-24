75594287d81aa6bcbe87ac2b24f1f9b04fe5bade
agral <adam@gralin.ski>
2025-07-24 13:10:42 +0200

    Split rendering and template handling to separate packages

--- Programming/Golang/ModernWebApplications/cmd/web/main.go
+++ Programming/Golang/ModernWebApplications/cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"ModernWebApplications/pkg/handlers"
 	"fmt"
 	"net/http"
 )
@@ -8,8 +9,8 @@ import (
 const PORT_NUMBER = ":31337"
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Printf("Starting application on port %s\n", PORT_NUMBER)
 	http.ListenAndServe(PORT_NUMBER, nil)
