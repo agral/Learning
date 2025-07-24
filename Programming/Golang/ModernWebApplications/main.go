d10b709e02a66c425313ba1d22778ed4b416889b
agral <adam@gralin.ski>
2025-07-24 13:03:44 +0200

    Extract the two handlers to proper file

--- Programming/Golang/ModernWebApplications/main.go
+++ Programming/Golang/ModernWebApplications/main.go
@@ -7,14 +7,6 @@ import (
 
 const PORT_NUMBER = ":31337"
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
