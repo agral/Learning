40ab4b1eb97b1bf3c434dfca187d055afc0b8299
agral <adam@gralin.ski>
2025-08-01 06:41:03 +0200

    Add ASan demo program "memory leak"

---
+++ Programming/Cpp/ASan/02_memory_leak.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <random>
+
+// A simple program that does vector addition,
+// but there's a memory leak - the arrays allocated with `new`
+// are never `delete`ed.
+int main() {
+    const int SIZE = 1024;
+    float *a = new float[SIZE];
+    float *b = new float[SIZE];
+
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution dist(0.0f, 1.0f);
+
+    std::generate(a, a+SIZE, [&]{ return dist(mt); });
+    std::generate(b, b+SIZE, [&]{ return dist(mt); });
+
+    for (int i = 0; i < SIZE; i++) {
+        a[i] += b[i];
+    }
+}
