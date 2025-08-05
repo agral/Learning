b783e01bea1a4f9805e8185a0006f2654c9e92d0
agral <adam@gralin.ski>
2025-08-05 06:15:00 +0200

    Use go-chi middleware Recoverer

--- Programming/Golang/ModernWebApplications/cmd/web/routes.go
+++ Programming/Golang/ModernWebApplications/cmd/web/routes.go
@@ -6,10 +6,12 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
