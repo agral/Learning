63750c06f4a11c318d8c63f52dc669cf8a18c41b
agral <adam@gralin.ski>
2025-10-01 12:19:50 +0200

    Finish up the dice game.

--- Programming/Web/WebDevUdemy/Dice/js/main.js
+++ Programming/Web/WebDevUdemy/Dice/js/main.js
@@ -1,10 +1,17 @@
-let roll1 = 1 + Math.floor(Math.random() * 6);
-let roll2 = 1 + Math.floor(Math.random() * 6);
-console.log(roll1)
-console.log(roll2)
-
 let i1 = document.getElementById("i1")
 let i2 = document.getElementById("i2")
+let r = document.getElementById("result")
+
+let roll1 = 1 + Math.floor(Math.random() * 6);
+let roll2 = 1 + Math.floor(Math.random() * 6);
 
 i1.src = "img/dice" + roll1 + ".png"
 i2.src = "img/dice" + roll2 + ".png"
+
+if (roll1 > roll2) {
+    r.innerText = "Player 1 wins!"
+} else if (roll1 < roll2) {
+    r.innerText = "Player 2 wins!"
+} else {
+    r.innerText = "That's a draw!"
+}
