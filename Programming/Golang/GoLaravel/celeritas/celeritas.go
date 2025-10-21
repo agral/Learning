349f13a43a8a026ab4616d25292b698607e44045
agral <adam@gralin.ski>
2025-10-21 11:48:31 +0200

    Write an initializer to create app directories

--- Programming/Golang/GoLaravel/celeritas/celeritas.go
+++ Programming/Golang/GoLaravel/celeritas/celeritas.go
@@ -1,13 +1,35 @@
 package celeritas
 
-func AddTwoInts(a, b int) int {
-	return a + b
+const VERSION = "1.0.0"
+
+type Celeritas struct {
+	AppName string
+	IsDebug bool
+	Version string
 }
 
-func SubTwoInts(a, b int) int {
-	return a - b
+func (c *Celeritas) New(rootPath string) error {
+	pathConfig := initPaths{
+		rootPath:    rootPath,
+		folderNames: []string{"handlers", "migrations", "views", "data", "public", "tmp", "logs", "middleware"},
+	}
+
+	err := c.Init(pathConfig)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func MulTwoInts(a, b int) int {
-	return a * b
+func (c *Celeritas) Init(p initPaths) error {
+	root := p.rootPath
+	for _, path := range p.folderNames {
+		err := c.CreateDirIfNotExist(root + "/" + path)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
--- Programming/Golang/GoLaravel/myapp/main.go
+++ Programming/Golang/GoLaravel/myapp/main.go
@@ -1,13 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/agral/celeritas"
-)
-
 func main() {
-	result := celeritas.AddTwoInts(1, 2)
-	result = celeritas.MulTwoInts(4, 5)
-	fmt.Println(result)
 }
