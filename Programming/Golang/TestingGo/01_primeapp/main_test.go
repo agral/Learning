618adff6349c14435e971504f7ed394ac094e1bc
agral <adam@gralin.ski>
2026-02-05 21:50:13 +0100

    Add another specific test with a prime number

--- Programming/Golang/TestingGo/01_primeapp/main_test.go
+++ Programming/Golang/TestingGo/01_primeapp/main_test.go
@@ -10,4 +10,13 @@ func Test_isPrime(t *testing.T) {
 	if msg != "0 is not prime by definition." {
 		t.Error("Wrong message returned:", msg)
 	}
+
+	result, msg = isPrime(13)
+	if result == false {
+		t.Errorf("With %d as test parameter, got `false`, but expected `true`", 13)
+	}
+	if msg != "13 is a prime number." {
+		t.Error("Wrong message returned:", msg)
+	}
+
 }
