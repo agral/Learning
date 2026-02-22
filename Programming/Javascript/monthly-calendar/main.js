0a73e2422e819d589cd0947b396a2f03103d4e2b
agral <adam@gralin.ski>
2026-02-22 22:20:49 +0100

    Highlight weekend days in blue

--- Programming/Javascript/monthly-calendar/main.js
+++ Programming/Javascript/monthly-calendar/main.js
@@ -1,5 +1,9 @@
 const calendar = document.getElementById("calendar");
 
+const isWeekend = day => {
+    return day % 7 === 0 || day % 7 === 6;
+}
+
 for (let day=1; day <= 31; day++) {
-    calendar.insertAdjacentHTML("beforeend", `<div class="day">${day}</div>`);
+    calendar.insertAdjacentHTML("beforeend", `<div class="day week${isWeekend(day) ? "end" : ""}">${day}</div>`);
 }
