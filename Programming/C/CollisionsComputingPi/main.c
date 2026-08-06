375d9aa0049532878849e22a37833b2e67225ca7
agral <adam@gralin.ski>
2026-08-06 23:25:54 +0200

    Add Raylib as dependency

--- Programming/C/CollisionsComputingPi/main.c
+++ Programming/C/CollisionsComputingPi/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <raylib.h>
 
 int main()
 {
     printf("Hello, world!\n");
+    InitWindow(800, 600, "PiBlocks");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
