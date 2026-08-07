e9ee37832a39807aceacb3b9ecbcf02089efda94
agral <adam@gralin.ski>
2026-08-07 09:09:35 +0200

    Draw a white rect on a black bg

--- Programming/C/CollisionsComputingPi/main.c
+++ Programming/C/CollisionsComputingPi/main.c
@@ -4,11 +4,13 @@
 int main()
 {
     printf("Hello, world!\n");
-    InitWindow(800, 600, "PiBlocks");
+    InitWindow(1024, 768, "PiBlocks");
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
         BeginDrawing();
+        ClearBackground(BLACK);
+        DrawRectangle(50, 50, 200, 200, WHITE);
         EndDrawing();
     }
 
