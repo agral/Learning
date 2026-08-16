eebe9a6e6173a104fe72346c6ac8fcb8ca5dc2e2
agral <adam@gralin.ski>
2026-08-16 12:02:30 +0200

    Draw the walls and the floor

--- Programming/C/CollisionsComputingPi/main.c
+++ Programming/C/CollisionsComputingPi/main.c
@@ -4,6 +4,7 @@
 const int WIDTH = 1024;
 const int HEIGHT = 768;
 const double FLOOR_LEVEL = 0.7 * HEIGHT;
+const double WALL_X = 50;
 
 typedef struct {
     int size;
@@ -11,6 +12,14 @@ typedef struct {
     double velX;
     double mass;
 } Block;
+
+void draw_scene() {
+    // Draw the floor:
+    DrawRectangle(WALL_X, FLOOR_LEVEL, WIDTH - WALL_X, 1, YELLOW);
+
+    // Draw the walls:
+     DrawRectangle(WALL_X, 0, 1, FLOOR_LEVEL, YELLOW);
+}
 
 int main()
 {
@@ -26,6 +35,8 @@ int main()
 
         BeginDrawing();
         ClearBackground(BLACK);
+
+        draw_scene();
 
         // Draw both blocks
         DrawRectangle(small.posX, FLOOR_LEVEL - small.size, small.size, small.size, WHITE);
