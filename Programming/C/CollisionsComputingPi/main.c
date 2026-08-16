2f47149456b3c0b77594938cf246ff5e29eed50a
agral <adam@gralin.ski>
2026-08-16 12:03:23 +0200

    Move the drawing of blocks to draw_scene

--- Programming/C/CollisionsComputingPi/main.c
+++ Programming/C/CollisionsComputingPi/main.c
@@ -13,21 +13,26 @@ typedef struct {
     double mass;
 } Block;
 
+Block small = (Block){50, 0.3 * WIDTH, 0, 1};
+Block big = (Block){200, 0.7 * WIDTH, -1, 10000};
+
+
 void draw_scene() {
     // Draw the floor:
     DrawRectangle(WALL_X, FLOOR_LEVEL, WIDTH - WALL_X, 1, YELLOW);
 
     // Draw the walls:
      DrawRectangle(WALL_X, 0, 1, FLOOR_LEVEL, YELLOW);
+
+    // Draw both blocks
+    DrawRectangle(small.posX, FLOOR_LEVEL - small.size, small.size, small.size, WHITE);
+    DrawRectangle(big.posX, FLOOR_LEVEL - big.size, big.size, big.size, WHITE);
 }
 
 int main()
 {
     printf("Hello, world!\n");
     InitWindow(WIDTH, HEIGHT, "PiBlocks");
-
-    Block small = (Block){50, 0.3 * WIDTH, 0, 1};
-    Block big = (Block){200, 0.7 * WIDTH, -1, 10000};
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
@@ -37,10 +42,6 @@ int main()
         ClearBackground(BLACK);
 
         draw_scene();
-
-        // Draw both blocks
-        DrawRectangle(small.posX, FLOOR_LEVEL - small.size, small.size, small.size, WHITE);
-        DrawRectangle(big.posX, FLOOR_LEVEL - big.size, big.size, big.size, WHITE);
 
         EndDrawing();
     }
