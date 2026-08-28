f8c0e8af58511f1dfd7df1ca27b6da768d342e58
agral <adam@gralin.ski>
2026-08-28 10:28:48 +0200

    Handle weekday and weekend days

--- Programming/Javascript/monthly-calendar/main.js
+++ Programming/Javascript/monthly-calendar/main.js
@@ -4,6 +4,14 @@ const isWeekend = day => {
     return day % 7 === 0 || day % 7 === 6;
 }
 
+const getWeekdayName = day => {
+    const date = new Date(todayYear, todayMonth, day);
+    const dayName = new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(date);
+}
+
+const today = new Date();
+const todayYear = today.getFullYear();
+const todayMonth = today.getMonth();
 for (let day=1; day <= 31; day++) {
     calendar.insertAdjacentHTML("beforeend", `<div class="day week${isWeekend(day) ? "end" : ""}">${day}</div>`);
 }
