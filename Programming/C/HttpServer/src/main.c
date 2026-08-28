4a04af33f74806c0561d4b3592898a792860422a
agral <adam@gralin.ski>
2026-08-28 10:25:28 +0200

    Define a file descriptor for the C server

--- Programming/C/HttpServer/src/main.c
+++ Programming/C/HttpServer/src/main.c
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 
 int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
     while (1) {
         // TODO: actually receive a request.
         printf("Waiting for a request...\n");
