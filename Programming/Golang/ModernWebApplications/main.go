21339a4b8e8be6f60a7102a18b92dd1b365fb35c
agral <adam@gralin.ski>
2026-09-14 09:32:22 +0200

    Split handlers and rendering from main

--- Programming/Golang/ModernWebApplications/main.go
+++ Programming/Golang/ModernWebApplications/main.go
@@ -2,19 +2,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 const PORT_NUMBER = ":8080"
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
 
 func main() {
 	http.HandleFunc("/", Home)
@@ -22,13 +13,4 @@ func main() {
 
 	fmt.Printf("Starting application on port %s\n", PORT_NUMBER)
 	http.ListenAndServe(PORT_NUMBER, nil)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template:", err)
-		return
-	}
 }
