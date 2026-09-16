2a38fc3d1479729bf620acf92022393d8e41d4ea
agral <adam@gralin.ski>
2026-09-16 11:34:59 +0200

    Switch to a more advanced templating system
    Implemented the more advanced templating system, as described
    in lesson #31 of the course.
    
    The new system is advertised as more robust and efficient.
    Instead I've noticed that the entire cache gets re-initialized
    (which is *expensive*) every time any page is loaded.
    But I'm sticking with that approach, just added some logging.
    Maybe there's something important I'm missing, or the lecturer
    will rectify it further on.

--- Programming/Golang/ModernWebApplications/pkg/render/render.go
+++ Programming/Golang/ModernWebApplications/pkg/render/render.go
@@ -1,68 +1,78 @@
 package render
 
 import (
-	"fmt"
+	"bytes"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
 // Renders actual templates using html/template functionalities.
-func RenderTemplateNoCaching(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
-	err := parsedTemplate.Execute(w, nil)
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	// Create a template cache
+	templateCache, err := createTemplateCache()
 	if err != nil {
-		fmt.Println("Error parsing template:", err)
-		return
+		// Instantiating all the templates has failed - just die at this point,
+		// it makes no sense to proceed.
+		log.Println("Failed to instantiate the template cache")
+		log.Fatal(err)
 	}
+
+	// Try getting the requested template from the template cache:
+	t, isOk := templateCache[tmpl]
+	if !isOk {
+		// Can't find the expected template in the cache - just die at this point.
+		log.Printf("Failed to access the template: %s\n", tmpl)
+		log.Fatal()
+	}
+
+	buffer := new(bytes.Buffer)
+	err = t.Execute(buffer, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Finally render the template:
+	_, err = buffer.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplateCache() (map[string]*template.Template, error) {
+	myCache := make(map[string]*template.Template)
+
+	// Get all files matching: ./templates/*.page.tmpl:
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	layouts, err := filepath.Glob("./templates/*.layout.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	// Process the found template files:
+	for _, page := range pages {
+		basename := filepath.Base(page)
+		templateSet, err := template.New(basename).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(layouts) > 0 {
+			templateSet, err = templateSet.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[basename] = templateSet
+	}
+
+	log.Printf("Returning a cache: %v", myCache)
+	return myCache, nil
 }
 
 var templateCache = make(map[string]*template.Template)
-
-func RenderTemplateSimpleCaching(w http.ResponseWriter, t string) {
-	var tmpl *template.Template
-	var err error
-
-	// check whether template is already in cache?
-	_, isOk := templateCache[t]
-	if !isOk {
-		// Need to intantiate the template & store it in the map
-		fmt.Println("Creating a new template")
-		err = createTemplateCache(t)
-		if err != nil {
-			fmt.Println(err)
-		}
-	} else {
-		// Template already available in the map
-		fmt.Println("Using cached template")
-	}
-
-	tmpl = templateCache[t]
-
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func createTemplateCache(t string) error {
-	templates := []string{
-		fmt.Sprintf("./templates/%s", t),
-		"./templates/base.layout.tmpl",
-	}
-
-	// Parse the template:
-	tmpl, err := template.ParseFiles(templates...)
-	if err != nil {
-		return err
-	}
-
-	// Add the parsed template to template cache:
-	templateCache[t] = tmpl
-
-	return nil
-}
-
-// Calls to RenderTemplate should from now on invoke RenderTemplateSimplified, not RenderTemplatePrevious.
-func RenderTemplate(w http.ResponseWriter, t string) {
-	RenderTemplateSimpleCaching(w, t)
-}
